8d7626d94fc17fc5954ccf607c1d9ba13145562f
Danil <105702087+Danisimo0@users.noreply.github.com>
2022-05-18 21:45:31 +0600

    calk

--- calc.py
+++ calc.py
@@ -13,59 +13,88 @@
 #print(result)
 
 
-def calc(number1, number2, oper):
-    print(number1, number2, oper)
-    if oper == "+":
+#def calc(number1, number2, oper):
+ #   print(number1, number2, oper)
+  #  if oper == "+":
+   #     return number1 + number2
+    #if oper == "-":
+#        return number1 - number2
+ #   if oper == "*":
+  #      return number1 * number2
+   # if oper == "/":
+    #    if number2 != 0:
+     #       return number1 / number2
+      #  else:
+       #     print("Вы ввели ноль")
+    #if oper == "**":
+     #   return number1 ** number2
+    #if oper == "//":
+        #if number2 != 0:
+       #     return number1 // number2
+      #  else:
+     #       print("Вы ввели ноль")
+    #        return (print("Введите другое число"))
+    import math
+    #if oper == "math.sqrt":
+   #     return math.sqrt(number1), math.sqrt(number2)
+  #  if oper == "%":
+ #       return number1 % number2
+
+
+
+
+#result = calc(10, 3, "+")
+#print(result)
+
+#result = calc(10, 3, "-")
+#print(result)
+
+#result = calc(10, 3, "*")
+#print(result)
+
+#result = calc(10, 0, "/")
+#print(result)
+#result = calc(10, 3, "**")
+#print(result)
+
+#result = calc(10, 3, "//")
+#print(result)
+
+#result = calc(64, 144, "math.sqrt")
+#print(result)
+
+#result = calc(10, 4, "%")
+#print(result)
+
+import math
+def calk(number1, operation, number2 = 10,):
+    print(number1, number2, operation)
+    if operation == "+":
         return number1 + number2
-    if oper == "-":
+    if operation == "-":
         return number1 - number2
-    if oper == "*":
+    if operation == "*":
         return number1 * number2
-    if oper == "/":
-        if number2 != 0:
+    if operation == "/":
+        if number1 != 0:
             return number1 / number2
         else:
-            print("Вы ввели ноль")
-    if oper == "**":
+            print("vy vveli 0")
+            return 0
+    if operation == "**":  # vozvedenie v stepen
         return number1 ** number2
-    if oper == "//":
-        if number2 != 0:
-            return number1 // number2
-        else:
-            print("Вы ввели ноль")
-            return (print("Введите другое число"))
-    import math
-    if oper == "math.sqrt":
-        return math.sqrt(number1), math.sqrt(number2)
-    if oper == "%":
+    if operation == "//":  # Delenie bez ostatka
+        return number1 // number2
+    if operation == "%":
         return number1 % number2
+    if operation == "///":
+        return math.sqrt(number1)
 
 
 
-
-result = calc(10, 3, "+")
+result = calk(9, "/",)
 print(result)
 
-result = calc(10, 3, "-")
-print(result)
-
-result = calc(10, 3, "*")
-print(result)
-
-result = calc(10, 0, "/")
-print(result)
-
-result = calc(10, 3, "**")
-print(result)
-
-result = calc(10, 3, "//")
-print(result)
-
-result = calc(64, 144, "math.sqrt")
-print(result)
-
-result = calc(10, 4, "%")
-print(result)
 
 
 
