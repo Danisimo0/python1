832ceb22423e01f4bd6aaff544d2514416e18724
Danil <105702087+Danisimo0@users.noreply.github.com>
2022-05-26 18:45:00 +0600

    update

--- home work1.py
+++ home work1.py
@@ -1,19 +1,20 @@
 from tkinter import *
 import tkinter.ttk as ttk1
-
 def knopka():
     print("Все работает хорошо")
 root = Tk()
+root.geometry('430x220+430+220')
 frm = ttk1.Frame(root, padding=100)
-root.title ("Заголовок окна программы")
+root.title("Заголовок окна программы")
 root.geometry("480x280")
-frm.grid()
-Button( text="Это кнопка",
+frm.pack()
+Button(text="Это кнопка",
          foreground="blue",  # цвет текста
          padx="100",  # отступ от границ до содержимого по горизонтали
-         pady="50",  # отступ от границ до содержимого по вертикали
+         pady="40",  # отступ от границ до содержимого по вертикали
          font="16",  # высота шрифта
-         command = knopka
-        ).grid(column=1, row=1)
+         command=knopka
+        ).pack(sid=TOP)
+
 
 root.mainloop()
