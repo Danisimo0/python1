3ea4dcf2a5e1b1521a4a07d384dbd27275de449d
Danil <105702087+Danisimo0@users.noreply.github.com>
2022-06-18 21:27:14 +0600

    Home work

--- Home_work/HW.1 папка с 1 текстовым файлом (.txt) в 5 строк/Home_work_txt.py
+++ Home_work/HW.1 папка с 1 текстовым файлом (.txt) в 5 строк/Home_work_txt.py
@@ -8,3 +8,14 @@ with open("folder/file.txt") as file:
         if len(line) > 1:
             new_list.append(line)
     print(new_list)
+
+try:
+    os.mkdir("folder/new")
+except:
+    print("folder already created")
+
+    for i in range(0, len(new_list)):
+        file_name = f'folder/new/file{i}.txt'
+        with open(file_name, 'w') as f:
+            f.write(new_list[i])
+    os.remove("folder/file.txt")
