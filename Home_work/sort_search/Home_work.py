d6dec20652bde73d522f4376cc244be1c0ff13f3
Danil <105702087+Danisimo0@users.noreply.github.com>
2022-07-07 15:35:40 +0600

    up

--- Home_work/sort_search/Home_work.py
+++ Home_work/sort_search/Home_work.py
@@ -1,15 +1,17 @@
-from collections import Counter
+import re
 
-str = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu magna sit amet neque lobortis dignissim. " \
-      "Mauris vehicula lacinia bibendum. Phasellus elementum ipsum et mi mollis, sed eleifend elit pharetra. Donec " \
-      "interdum tempus ligula non vulputate. Cras mollis rhoncus facilisis. Fusce at viverra magna, id tempor nulla. " \
-      "Quisque at felis eget arcu gravida efficitur eget ac enim. Etiam quisque efficitur lorem at lorem dictum, a " \
-      "sagittis ipsum pulvinar. Maecenas elit nisi, iaculis a dolor id, tempor molestie dolor. Pellentesque aliquet non " \
-      "orci at convallis. Donec laoreet nisl quam. Ut accumsan, dui ut mattis ultricies, est nulla semper est, eget " \
-      "pulvinar magna lacus ut risus."
+str = \
+    " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu magna sit amet neque lobortis dignissim. " \
+    "Mauris vehicula lacinia bibendum. Phasellus elementum ipsum et mi mollis, sed eleifend elit pharetra. Donec " \
+    "interdum tempus ligula non vulputate. Cras mollis rhoncus facilisis. Fusce at viverra magna, id tempor nulla. " \
+    "Quisque at felis eget arcu gravida efficitur eget ac enim. Etiam quisque efficitur lorem at lorem dictum, a " \
+    "sagittis ipsum pulvinar. Maecenas elit nisi, iaculis a dolor id, tempor molestie dolor. " \
+    "Pellentesque aliquet non " \
+    "orci at convallis. Donec laoreet nisl quam. Ut accumsan, dui ut mattis ultricies, est nulla semper est, eget " \
+    "pulvinar magna lacus ut risus."
 " Duis sed hendrerit odio. Etiam scelerisque nunc quis placerat interdum." \
 "Nam condimentum enim ac justo fermentum, et"
-"imperdiet purus finibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris bibendum" \
+"imper-diet purus finibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris bibendum" \
 "urna vitae ullamcorper"
 "pellentesque. Aenean in est vitae felis semper vulputate convallis eu quam. Nullam feugiat elementum libero. " \
 "Donec scelerisque finibus laoreet. Nam eu risus facilisis, iaculis urna vitae, aliquam neque. " \
@@ -21,5 +23,10 @@ str = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu magna
 "libero ut dui efficitur malesuada. Donec ultricies vel purus vel pellentesque. Etiam fusce a ex in" \
 " libero rutrum placerat suscipit ac tellus. Etiam dignissim ullamcorper tincidunt." \
 " Proin tempor lorem eu nulla euismod, id sollicitudin massa ultricies. "
-counter = Counter(str)
-print(counter['etiam'])
+
+substring = "Etiam"
+
+# count = str.count(substring)
+# print(count)
+
+print(len(re.findall("Etiam", str)))
