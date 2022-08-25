631fe3e2a9cce7b44411f836e96620d093c4f369
Danil <105702087+Danisimo0@users.noreply.github.com>
2022-08-25 14:16:09 +0600

    HW

---
+++ Home_work/Не знаю как назвать ДЗ.py
@@ -0,0 +1,85 @@
+# a = int(input("Введите 1 число: "))
+# b = int(input("Введите 2 число: "))
+# c = int(input("Введите 3 число: "))
+#
+# summa = a+b+c
+# mult = a*b*c
+# print("Summa: ", summa)
+# print("Mult: ", mult)
+
+# Exercise 2
+
+# zp = int(input("Введите 1 число: "))
+# bank = int(input("Введите 2 число: "))
+# zadoljnast = int(input("Введите 3 число: "))
+# dohod = zp-bank-zadoljnast
+# print("Octatok: ", dohod)
+
+
+# Exercise 3
+
+# a = float(input("Введите 1 диагональ: "))
+# b = float(input("Введите 2 диагональ: "))
+# s = a*b*0.5
+# print("S = ", s)
+
+
+# Exercise 4
+
+# print("To be\nor not\nto be")
+
+# Exercise 5
+
+# print("«Life is what happens\nwhen\nyou're busy making other plans»\nJohn Lennon ")
+
+# Exercise 6
+
+# a = int(input("Введите 1 число: "))
+# b = int(input("Введите 2 число: "))
+# c = int(input("Введите 3 число: "))
+#
+# vmeste = ('{0}' + '{1}' + '{2}').format(a, b, c)
+# print(vmeste)
+
+
+# Exercise 7
+
+# a = int(input("Введите 1 число: "))
+# b = int(input("Введите 2 число: "))
+# c = int(input("Введите 3 число: "))
+# d = int(input("Введите 4 число: "))
+# res = a * b * c * d
+# print(" Res = ", res)
+
+
+# Exercise 8
+
+
+
+# m = float(input('Metrov : '))
+#
+# print(f'{m} м = {0.000621371 * m} мил')
+#
+# print(f'{m} м = {10 * m} дм')
+#
+# print(f'{m} м = {100 * m} см')
+#
+# print(f'{m} м = {1000 * m} мм')
+
+
+
+# Exercise 9
+
+# a = int(input(" a = "))
+# h = int(input("h = "))
+# s = a * h/2
+# print("S = ", s)
+
+
+# Exercise 10
+
+
+number = input("Введите 4-х значное число: ")
+number = number[::-1]
+print(number)
+
