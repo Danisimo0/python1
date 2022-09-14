7e18f52f9abd9d184818307072ca13033b6ae76e
Danil <105702087+Danisimo0@users.noreply.github.com>
2022-09-14 22:21:40 +0600

    CW

--- Home_work/HW.Aiohttp/Aiohttp.py
+++ Home_work/HW.Aiohttp/Aiohttp.py
@@ -1,55 +1,46 @@
 import asyncio
 import requests
 from bs4 import BeautifulSoup
+import aiohttp
+import os
+
+# 1
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/102.0.0.0 Safari/537.36'
+}
+url = 'https://kurs.kz'
+response = requests.get(urlm, headers=headers)
+# print(response)
+
+bs = BeautifulSoup(response.text, "lxml")
+# print(bs)
 
 
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-#                   'AppleWebKit/537.36 (KHTML, like Gecko) '
-#                   'Chrome/102.0.0.0 Safari/537.36'
-# }
+print(Search)
+
+# 2
+
+# photo = requests.get(url="https://jsonplaceholder.typicode.com/photos")
+#
+# photos_list = photo.json()
+#
+# for key, val in photos_list.items():
+#     print(f"{key}: {val}")
+#
 
 
-
-# response = requests.get(url=" ", headers=headers)
-# print(response.text)
-
+r = requests.get('https://jsonplaceholder.typicode.com/photos')
+html = r.text
 
 
-def multi():
-    current_thread = 16
-    with concurrent.futures.ThreadPoolExecutor(max_workers=current_thread * 2 + 1) as executor:
-        for x in range(1, 10 + 1):
-            executor.submit(get_name, f'img{x}.jpg')
+def find(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find('div', class_='cards')
+    row = div.find_all('img')
 
+    links = [col['src'][2:] for col in row]
 
-async def multi_async():
-    await asyncio.gather(*[get_name_async(f"img{i}.jpg") for i in range(1, 10 + 1)])
-
-
-async def get_name_async(file_name: str) -> None:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url="https://picsum.photos/600/600/", headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                          'AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/102.0.0.0 Safari/537.36'
-        }
-
-                               ) as request:
-            response = await request.read()
-
-    with open(file_name, "wb") as file:
-        file.write(response)
-
-
-def async_start():
-    asyncio.run(multi_async())
-
-
-if __name__ == '__main__':
-    # get_name()
-    # multi()
-    async_start()
-
-
-
+    return links
