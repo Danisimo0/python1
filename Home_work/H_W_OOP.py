f2a5a90521c08f41f82404f66914c40e6fe055c7
Danil <105702087+Danisimo0@users.noreply.github.com>
2022-09-17 00:56:26 +0600

    hw

--- Home_work/H_W_OOP.py
+++ Home_work/H_W_OOP.py
@@ -2,26 +2,98 @@ import requests
 import json
 import os
 
-# много пыхтел, многое опробовал, но все равно не понял в чем ошибка и тд, сначала была ошибка, а теперь None
+
+# много пыхтел, многое опробовал, но все равно не понял в чем ошибка и тд,получаю только json объект
 
 
-def download_img(url='https://jsonplaceholder.typicode.com/photos'):
+
+def download_img(url='https://jsonplaceholder.typicode.com/photos/1'):
     try:
         response = requests.get(url=url)
-        with open('file_name', 'wb') as file:
+        with open('new.json', 'wb') as file:
             file.write(response.content)
+
+        with open('new.json', mode='w') as file:
+            dict3 = {
+                "userId": 1,
+                "id": 1,
+                "title": "delectus aut autem",
+                "completed": [
+                    {
+                        "userId": 1,
+                        "id": 1,
+                        "title": "delectus aut autem",
+                        "completed": False
+                    },
+                    {
+                        "userId": 1,
+                        "id": 1,
+                        "title": "delectus aut autem",
+                        "completed": False
+                    },
+                    {
+                        "userId": 1,
+                        "id": 1,
+                        "title": "delectus aut autem",
+                        "completed": False
+                    }
+                ]
+            }
+            # есть предположение что здесь не правильно записываю
+
+            ret = json.dumps(dict3)
+            print(ret)
+            file.write(ret)
+
 
     except Exception as _ex:
         return 'error'
 
 
-def main():
-    print(download_img(url='https://jsonplaceholder.typicode.com/photos'))
+download_img()
 
 
-if __name__ == '__main__':
-    main()
 
+# ниже все мои попытки не верные
+
+
+# def download_img(url='https://jsonplaceholder.typicode.com/photos'):
+#     photo = requests.get(url)
+#     photos_list = photo.json()
+#     for photo in photo_list:
+#         with open(f'Home_work/photo{photo["id"]}.json', mode="w") as file:
+#             json.dump(photo)
+#     with open("photo.json", mode="rb") as file:
+#         json1 = json.load(file)
+#         print(json1)
+#
+#
+# download_img()
+
+# photo = requests.get(url="https://jsonplaceholder.typicode.com/photos%22")
+# print((photo.json()))
+#
+# photos_list = photo.json()
+# for photo in photos_list:
+#     with open(f"python_photo/photo{photo['id']}.json", mode="w") as file:
+#         json.dump((photo), fp=file)
+#
+# with open("photo.json", mode="rb") as file:
+#     json1 = json.load(file)
+#     print(json1)
+
+
+# def download_img(url='https://jsonplaceholder.typicode.com/photos'):
+#     try:
+#         response = requests.get(url=url)
+#         with open('new2.img', 'wb') as file:
+#             file.write(response.content)
+#
+#     except Exception as _ex:
+#         return 'error'
+#
+#
+# download_img()
 
 # # url = "https://jsonplaceholder.typicode.com/photos/1"
 # #
@@ -41,14 +113,7 @@ if __name__ == '__main__':
 #
 #
 #
-#
 
-# import json
-# import requests
-#
-
-# # HTTP  - GET POST PUT(PUTCH) DELETE (CRUD)
-#
 # response = requests.get(url="https://jsonplaceholder.typicode.com/todos/1")
 # print(response)
 #
@@ -93,7 +158,6 @@ if __name__ == '__main__':
 # file.write(ret)
 
 
-
 # def download_img(url='https://jsonplaceholder.typicode.com/photos'):
 #     file_name = url.split("/")[-1]
 #     print(file_name)
@@ -102,5 +166,3 @@ if __name__ == '__main__':
 #
 #
 # download_img()
-
-
