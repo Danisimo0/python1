52847ebecd47c17a47889234f353331abb118a85
Danil <105702087+Danisimo0@users.noreply.github.com>
2022-10-18 16:25:39 +0600

    hw

--- Home_work/Hw_UI/UI_lib.py
+++ Home_work/Hw_UI/UI_lib.py
@@ -1,3 +1,16 @@
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello world</p>"
+# if __name__ == "__main__":
+#     app.run()
+
+
+# Task 2
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,4 +18,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello world</p>"
+    text = ""
+    for i in range(1, 667):
+        text += f"<p>{i}</p>"
+    return text
+
+
+if __name__ == "__main__":
+    app.run()
