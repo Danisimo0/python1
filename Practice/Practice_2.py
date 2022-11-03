a106011cb91ca9ce26c4c4182de2264abaea9bfc
Danil <105702087+Danisimo0@users.noreply.github.com>
2022-11-03 13:26:44 +0600

    exam

--- Practice/Practice_2.py
+++ Practice/Practice_2.py
@@ -1,3 +1,48 @@
+# Есть файл (ШАБЛОН), Excel,в который нужно заполнить данные, сохраняя дизайн из шаблона, сами данные нужно сгенирировать range 5-5
+# в центр, при измене шаблона, код должен не падать,
+
+import openpyexcel
+
+workbook = openpyexcel.load_workbook("dist/file_.xlsx")
+worksheet = workbook.active
+
+# print(worksheet.cell(1, 1).value)
+
+max_row = worksheet.max_row
+max_column = worksheet.max_column
+
+for row in range(1, max_row + 1):
+    for column in range(1, max_column + 1):
+        match str(worksheet.cell(row, column).value):
+            case "Js":
+                worksheet.cell(row=row, column=column)
+            case "Java":
+                worksheet.cell(row=row, column=column)
+            case "Python":
+                worksheet.cell(row=row, column=column)
+        print(f"{row} {column} {worksheet.cell(row, column).value}")
+workbook.save("dist/file_.xlsx")
+workbook.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Получить данные jsonplaceholder posts, записать в 1 https://jsonplaceholder.typicode.com/todos/1
 # import requests
 # import json
@@ -10,45 +55,42 @@
 
 #     json_1.write(response.json())
 
-
-
-from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow,QGridLayout
-import sys
-import time
-
-
-class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("My App")
-
-        self.button = QPushButton("Press Me!")
-        self.button.clicked.connect(self.timer)
-        layout = QGridLayout()
-        layout.addWidget(self.button, 2, 0, 1, 1)
-        self.setFixedSize(QSize(480, 300))
-
-
-        # Устанавливаем центральный виджет Window.
-        self.setCentralWidget(self.button)
-
-
-    def timer(self):
-       index = 1
-       while True:
-            index+=1
-            time.sleep(0.5)
-            print(index)
-
-
-app = QApplication(sys.argv)
-window = Window()
-window.show()
-app.exec()
-
-import sys
+#
+# from PyQt6.QtCore import QSize
+# from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QGridLayout
+# import sys
+# import time
+#
+#
+# class Window(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.setWindowTitle("My App")
+#
+#         self.button = QPushButton("Press Me!")
+#         self.button.clicked.connect(self.timer)
+#         layout = QGridLayout()
+#         layout.addWidget(self.button, 2, 0, 1, 1)
+#         self.setFixedSize(QSize(480, 300))
+#
+#         # Устанавливаем центральный виджет Window.
+#         self.setCentralWidget(self.button)
+#
+#     def timer(self):
+#         index = 1
+#         while True:
+#             index += 1
+#             time.sleep(0.5)
+#             print(index)
+#
+#
+# app = QApplication(sys.argv)
+# window = Window()
+# window.show()
+# app.exec()
+#
+# import sys
 
 # from PyQt6.QtCore import Qt
 # from PyQt6.QtWidgets import (
@@ -157,7 +199,6 @@ import sys
 #     sys.exit(app.exec())
 
 
-
 # class MainWindow(QMainWindow):
 #
 #     def __init__(self):
@@ -179,6 +220,6 @@ import sys
 #         print(s == Qt.CheckState.Checked)
 #         print(s)
 #
-#
-# Есть файл (ШАБЛОН), Excel,в который рнужно заполнить данные, сохраняя дизайн из шаблона, сами данные нужно сгенирировать range 5-5
-# в центр, при измене шаблона, код должен не падать, 
+
+
+
